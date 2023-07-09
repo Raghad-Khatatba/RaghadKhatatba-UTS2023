10ddac07f63e071c863d69361023d5f5211ba34d
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-07-09 18:13:02 +0300

    Task14 chrom DT (S14)

---
+++ chrome DT (S14)/main.js
@@ -0,0 +1,19 @@
+function logInfo() {
+    console.log('My Info');
+  }
+  
+  function logWarning() {
+    console.warn('My Warning');
+  }
+  
+  function logError() {
+    console.error('My Error');
+  }
+  
+  function logTable() {
+    console.table([
+      { Name: 'maha', Age: 25 },
+      { Name: 'Raghad', Age: 21 },
+      { Name: 'Shahed', Age: 19 }
+    ]);
+  }
